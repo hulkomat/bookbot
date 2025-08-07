c940b4b27e26b277c3e277f54a7fe3d952b05089
hulkomat <hulkomat@users.noreply.github.com>
2025-08-07 20:34:10 +0200

    refactor: update main script to accept dynamic book paths and enhance Justfile for easier execution

--- justfile
+++ justfile
@@ -6,3 +6,18 @@
 
 run:
     python3 main.py
+
+run-with-param path="dracula.txt":
+    python3 main.py books/{{path}}
+
+run-for-frankenstein:
+    just run-with-param frankenstein.txt
+
+run-for-pride-and-prejudice:
+    just run-with-param pride-and-prejudice.txt
+
+run-for-moby-dick:
+    just run-with-param moby-dick.txt
+
+run-for-dracula:
+    just run-with-param dracula.txt
--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from stats import get_word_count, sorted_dict_by_count
 from stats import get_character_count
 
+import sys
+
 def get_book_text(filepath: str):
     with open(filepath) as f:
         return f.read()
     
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+
+    path = sys.argv[1]
+    book_text = get_book_text(path)
     # print(book_text)
     num_words = get_word_count(book_text)
     character_dictionary = get_character_count(book_text)
     sorted_list = sorted_dict_by_count(character_dictionary)
 
     print(f"============ BOOKBOT ============\n"
-          f"Analyzing book found at books/frankenstein.txt...\n"
+          f"Analyzing book found at {path}...\n"
           f"----------- Word Count ----------\n"
           f"Found {num_words} total words\n"
           f"--------- Character Count -------")
